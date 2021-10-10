1b7134a765996b91fcfa670bf7c11773c292d044
rakibulalam9200 <rakibul9200@gmail.com>
2021-10-10 14:34:34 +0600

    start-firebase

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBos4SYISdT3j-LvLR9_uqlY4ZvM4Ben_Y",
+  authDomain: "ema-jhon-simple-487dd.firebaseapp.com",
+  projectId: "ema-jhon-simple-487dd",
+  storageBucket: "ema-jhon-simple-487dd.appspot.com",
+  messagingSenderId: "695970051020",
+  appId: "1:695970051020:web:00181951fb4612b406e369",
+};
+
+export default firebaseConfig;
